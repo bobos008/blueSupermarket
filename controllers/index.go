dc988edd5beff5fe0d38cf1188becff4b56234e9
bobos008 <501919337@qq.com>
2019-09-04 11:01:58 +0800

    修改生日字符传成datetime

--- controllers/index.go
+++ controllers/index.go
@@ -53,17 +53,7 @@ func (c *UserAddDataController) UserAddData() {
 	gender := c.GetString("gender")
 	phone := c.GetString("phone")
 	birthday := c.GetString("birthday")
-	/*loc, _ := time.LoadLocation("Local")
-	the_time, err := time.ParseInLocation("2006-01-02 15:04:05", birthday + " 00:00:00", loc)
-	var unix_time int64
-	if err == nil {
-		unix_time = the_time.Unix()
-	} else {
-		fmt.Println(err)
-		unix_time = time.Now().Unix()
-	}
-	time_date := time.Unix(unix_time, 0)*/
-	time_date,err := time.Parse("2006-01-02 15:04:05", birthday + " 00:00:00")
+	time_date,err := time.Parse("2006-01-02", birthday)
 	if err != nil {
 		fmt.Println(err)
 		time_date = time.Now()
