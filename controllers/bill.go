46ffec8fb73e84c32fed81e6d07f3cfb48163f41
bobos008 <501919337@qq.com>
2019-09-16 13:35:15 +0800

    账单管理部分功能

--- controllers/bill.go
+++ controllers/bill.go
@@ -223,6 +223,7 @@ func (c *BillUpdateController) BillUpdate() {
 	} else {
 		oneMap := make(map[string]interface{})
 		for _, bVal := range billSlice {
+			oneMap["Id"] = bVal.Id
 			oneMap["GoodsName"] = bVal.GoodsName
 			oneMap["GoodsNumber"] = bVal.GoodsNumber
 			oneMap["GoodsUnit"] = bVal.GoodsUnit
@@ -237,17 +238,68 @@ func (c *BillUpdateController) BillUpdate() {
 }
 
 func (c *BillUpdateDataController) BillUpdateData() {
-	providerId := c.GetString("providerId")
-	providerName := c.GetString("providerName")
-	people := c.GetString("people")
-	phone := c.GetString("phone")
-	Amount := c.GetString("Amount")
+	id := c.GetString("id")
+	intId, idErr := strconv.ParseInt(id, 10, 64)
+	if idErr != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}
+	goodsId := c.GetString("providerId")
+	goodsName := c.GetString("providerName")
+	goodsUnit := c.GetString("people")
+	billNum := c.GetString("phone")
+	goodsCount,err := strconv.ParseUint(billNum, 10, 32)
+	if err != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}
+	amount := c.GetString("Amount")
+	amountFloat64, amountErr := strconv.ParseFloat(amount,64)
+	if amountErr != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}
+	/*money, moneyErr := strconv.ParseFloat(fmt.Sprintf("%.2f", amountFloat64), 32)
+	if moneyErr != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}*/
 	supplier := c.GetString("supplier")
-	zhifu := c.GetString("zhifu")
+	providerId, providerErr := strconv.ParseInt(supplier,10, 64)
+	if providerErr != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}
+	zhiFu := c.GetString("zhifu")
+	isPay,zhiFuErr := strconv.ParseBool(zhiFu)
+	if zhiFuErr != nil {
+		c.Data["json"] = false
+		c.ServeJSON()
+		return
+	}
 
 	o := orm.NewOrm()
-
-
-	c.Data["json"] = true
+	bill := Bill{Id:intId}
+	if o.Read(&bill) == nil {
+		bill.GoodsNumber = goodsId
+		bill.GoodsName = goodsName
+		bill.GoodsUnit = goodsUnit
+		bill.GoodsCount = uint32(goodsCount)
+		bill.IsPay = isPay
+		bill.Amount = amountFloat64
+		bill.Provider = &Provider{Id:providerId}
+		_,updateErr := o.Update(&bill)
+		if updateErr == nil {
+			c.Data["json"] = true
+			c.ServeJSON()
+			return
+		}
+	}
+	c.Data["json"] = false
 	c.ServeJSON()
 }
